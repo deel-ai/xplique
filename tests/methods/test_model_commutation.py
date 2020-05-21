897243dab17324a2b7507a8d3dd53a2ffeb7941d
Thomas FEL <thomas.fel@protonmail.com>
2020-05-21 16:29:54 +0200

    utils: introduce relu override mechanism

---
+++ tests/methods/test_model_commutation.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+from xplique.methods.utils import guided_relu, override_relu_gradient
+
+def test_override_commutation():
+    """Ensure we commute correctly differents ReLU activations"""
+
+    model_1 = tf.keras.models.Sequential()
+    model_1.add(tf.keras.layers.Activation('relu'))
+
+    model_2 = tf.keras.models.Sequential()
+    model_2.add(tf.keras.layers.ReLU())
+
+    model_3 = tf.keras.models.Sequential()
+    model_3.add(tf.keras.layers.Activation(tf.nn.relu))
+
+    for model in [model_1, model_2, model_3]:
+        guided_model = override_relu_gradient(model, guided_relu)
+        assert guided_model.layers[0].activation == guided_relu
--- xplique/methods/utils.py
+++ xplique/methods/utils.py
@@ -3,6 +3,8 @@ Utils related to differents methods
 """
 
 import tensorflow as tf
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.models import clone_model
 
 
 def sanitize_input_output(explanation_method):
@@ -23,3 +25,95 @@ def sanitize_input_output(explanation_method):
         return explanations.numpy()
 
     return sanitize
+
+
+@tf.custom_gradient
+def guided_relu(inputs):
+    """
+    Guided relu activation function.
+    Act like a relu during forward pass, but allows only positive gradients with positive
+    activation to pass through during backprop.
+
+    Parameters
+    ----------
+    inputs : tf.Tensor
+        Input tensor
+
+    Returns
+    -------
+    output : tf.Tensor
+        Tensor, output or relu transformation.
+    grad_func : function
+        Gradient function for guided relu.
+    """
+
+    def grad_func(grads):
+        gate_activation = tf.cast(inputs > 0.0, tf.float32)
+        return tf.nn.relu(grads) * gate_activation
+
+    return tf.nn.relu(inputs), grad_func
+
+
+def is_relu(layer):
+    """
+    Check if a layer is a ReLU layer
+
+    Parameters
+    ----------
+    layer : tf.keras.layers.Layers
+
+    Returns
+    -------
+    is_relu : bool
+    """
+    if isinstance(layer, tf.keras.layers.ReLU):
+        return True
+    return False
+
+
+def has_relu_activation(layer):
+    """
+    Check if a layer has a ReLU activation.
+
+    Parameters
+    ----------
+    layer : tf.keras.layers.Layers
+
+    Returns
+    -------
+    has_relu: bool
+
+    """
+    if not hasattr(layer, 'activation'):
+        return False
+    return layer.activation in [tf.nn.relu, tf.keras.activations.relu]
+
+
+def override_relu_gradient(model, relu_policy):
+    """
+    Given a model, commute all original ReLU by a new given ReLU policy.
+
+    Parameters
+    ----------
+    model : tf.keras.model
+        Model to commute.
+    relu_policy : tf.custom_gradient
+        Function wrapped with custom_gradient, defining the ReLU backprop behaviour.
+
+    Returns
+    -------
+    model_commuted : tf.keras.model
+    """
+
+    def clone_func(layer):
+        if is_relu(layer):
+            return Activation(relu_policy)
+
+        if has_relu_activation(layer):
+            layer.activation = relu_policy
+
+        return layer
+
+    model_commuted = clone_model(model, clone_function=clone_func)
+
+    return model_commuted
