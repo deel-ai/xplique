5fa88a8b14c437e53d42a7e0d019784927aa122c
Thomas FEL <thomas.fel@protonmail.com>
2020-10-06 22:20:36 +0200

    methods: function args as tf.tensor instead of ndarray

--- xplique/methods/gradient_input.py
+++ xplique/methods/gradient_input.py
@@ -24,7 +24,7 @@ class GradientInput(BaseExplanation):
         inputs : ndarray (N, W, H, C)
             Input samples, with N number of samples, W & H the sample dimensions, and C the
             number of channels.
-        labels : ndarray(N, L)
+        labels : ndarray (N, L)
             One hot encoded labels to compute for each sample, with N the number of samples, and L
             the number of classes.
 
@@ -44,10 +44,10 @@ class GradientInput(BaseExplanation):
         ----------
         model : tf.keras.Model
             Model used for computing explanations.
-        inputs : ndarray (N, W, H, C)
+        inputs : tf.tensor (N, W, H, C)
             Input samples, with N number of samples, W & H the sample dimensions, and C the
             number of channels.
-        labels : ndarray(N, L)
+        labels : tf.tensor (N, L)
             One hot encoded labels to compute for each sample, with N the number of samples, and L
             the number of classes.
         batch_size : int
