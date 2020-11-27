b8fbcb9a0e977481858ae206546ca82617909953
Thomas FEL <thomas_fel@brown.edu>
2020-11-27 17:20:27 +0100

    utils: introduce find_layer helper

--- xplique/utils/__init__.py
+++ xplique/utils/__init__.py
@@ -3,5 +3,6 @@ Utility classes and functions
 """
 
 from .data_conversion import sanitize_input_output
-from .model_override import guided_relu, deconv_relu, override_relu_gradient
+from .model_override import guided_relu, deconv_relu, override_relu_gradient, \
+                            find_layer
 from .tf_operations import repeat_labels
