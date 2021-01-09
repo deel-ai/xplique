e24b19e8b7f9d13cdf51c1439082e9a1d5b44ded
Thomas FEL <thomas_fel@brown.edu>
2021-01-09 02:46:08 +0100

    feature viz: introduce optimisation mechanism

--- xplique/features_visualizations/__init__.py
+++ xplique/features_visualizations/__init__.py
@@ -8,3 +8,4 @@ from .transformations import random_blur, random_jitter, random_scale, \
                              random_flip, pad, compose_transformations
 from .preconditioning import fft_image, get_fft_scale, fft_to_rgb
 from .objectives import Objective
+from .optim import optimize
