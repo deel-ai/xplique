ef22dc67ea9722875685ae601222f2d16f48d1ca
Thomas FEL <thomas_fel@brown.edu>
2021-02-05 23:55:37 +0100

    tests: dispenses the commutation of relu layers

--- tests/attributions/test_gradients_override.py
+++ tests/attributions/test_gradients_override.py
@@ -5,16 +5,16 @@ from xplique.utils import guided_relu, override_relu_gradient
 
 def test_override_commutation():
     """Ensure we commute correctly differents ReLU activations"""
+    input_shape = (28, 28, 1)
 
     model_1 = tf.keras.models.Sequential()
     model_1.add(tf.keras.layers.Activation('relu'))
+    model_1.build(input_shape)
 
     model_2 = tf.keras.models.Sequential()
-    model_2.add(tf.keras.layers.ReLU())
+    model_2.add(tf.keras.layers.Activation(tf.nn.relu))
+    model_2.build(input_shape)
 
-    model_3 = tf.keras.models.Sequential()
-    model_3.add(tf.keras.layers.Activation(tf.nn.relu))
-
-    for model in [model_1, model_2, model_3]:
+    for model in [model_1, model_2]:
         guided_model = override_relu_gradient(model, guided_relu)
         assert guided_model.layers[0].activation == guided_relu
