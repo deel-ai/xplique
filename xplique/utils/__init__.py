a9f8087e0dc4c019319ebfa8cb0cf7a3c79a9f18
lucas Hervier <lucas.hervier@irt-saintexupery.com>
2021-06-21 18:38:29 +0200

    utils: introduce plot_explanation

--- xplique/utils/__init__.py
+++ xplique/utils/__init__.py
@@ -6,3 +6,4 @@ from .data_conversion import sanitize_input_output
 from .model_override import guided_relu, deconv_relu, override_relu_gradient, \
                             find_layer
 from .tf_operations import repeat_labels
+from .plot_explanation import plot_explanation
