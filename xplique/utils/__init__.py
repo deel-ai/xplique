676cbd56a40e855be415ba2aedc2389cc012d277
Thomas FEL <thomas_fel@brown.edu>
2021-06-26 20:40:20 +0200

    utils: move batch inference operations for common usage

--- xplique/utils/__init__.py
+++ xplique/utils/__init__.py
@@ -5,5 +5,5 @@ Utility classes and functions
 from .data_conversion import sanitize_input_output
 from .model_override import guided_relu, deconv_relu, override_relu_gradient, \
                             find_layer
-from .tf_operations import repeat_labels
+from .tf_operations import repeat_labels, batch_gradient, batch_predictions_one_hot
 from .plot_explanation import plot_explanation
