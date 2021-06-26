bc4b45361dba0059d2fbfcb1c18c3922cdcd3293
Thomas FEL <thomas_fel@brown.edu>
2021-06-26 20:43:13 +0200

    test: enforce float32 for the mockup data

--- tests/utils.py
+++ tests/utils.py
@@ -4,7 +4,7 @@ from tensorflow.keras.layers import Dense, Conv2D, Activation, Dropout, Flatten,
 from tensorflow.keras.utils import to_categorical
 
 def generate_data(x_shape=(32, 32, 3), num_labels=10, samples=100):
-    x = np.random.rand(samples, *x_shape)
+    x = np.random.rand(samples, *x_shape).astype(np.float32)
     y = to_categorical(np.random.randint(0, num_labels, samples), num_labels)
 
     return x, y
@@ -12,14 +12,11 @@ def generate_data(x_shape=(32, 32, 3), num_labels=10, samples=100):
 def generate_model(input_shape=(32, 32, 3), output_shape=10):
     model = Sequential()
     model.add(Input(shape=input_shape))
-    model.add(Conv2D(4, kernel_size=(3, 3),
+    model.add(Conv2D(4, kernel_size=(2, 2),
                      activation='relu'))
-    model.add(Conv2D(4, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
     model.add(Flatten())
-    model.add(Dense(4, activation='relu'))
-    model.add(Dropout(0.1))
     model.add(Dense(output_shape))
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='sgd')
@@ -30,5 +27,3 @@ def generate_model(input_shape=(32, 32, 3), output_shape=10):
 def almost_equal(arr1, arr2, epsilon=1e-6):
     """Ensure two array are almost equal at an epsilon"""
     return np.sum(np.abs(arr1 - arr2)) < epsilon
-
-
