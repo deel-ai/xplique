bf8a95b38ceb762b15f9a67b588951c3e4efee8d
lucas Hervier <lucas.hervier@irt-saintexupery.com>
2021-07-12 22:48:05 +0200

    plot: add new plots possibility for images

--- xplique/commons/__init__.py
+++ xplique/commons/__init__.py
@@ -6,4 +6,5 @@ from .data_conversion import tensor_sanitize, numpy_sanitize
 from .model_override import guided_relu, deconv_relu, override_relu_gradient, \
                             find_layer
 from .tf_operations import repeat_labels, batch_gradient, batch_predictions_one_hot
-from .plot_explanation import plot_explanation
+from .plot_explanation import plot_img_explanation, plot_several_images_explanations, \
+                              plot_image_several_explanations
