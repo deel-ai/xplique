7b21f12ac44be0dc68fcaee407e69b04c2062341
Thomas FEL <thomas_fel@brown.edu>
2021-07-30 16:03:56 +0200

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as fh:
 
 setup(
     name="Xplique",
-    version="0.1.0",
+    version="0.2.0",
     description="Explanations toolbox for Tensorflow 2",
     long_description=README,
     long_description_content_type="text/markdown",
