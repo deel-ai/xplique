764cc33d086249f49b592422005789301e857b91
Thomas FEL <thomas_fel@brown.edu>
2021-09-13 00:47:59 +0200

    utils: provision of a batching tool

--- xplique/commons/__init__.py
+++ xplique/commons/__init__.py
@@ -5,4 +5,4 @@ Utility classes and functions
 from .data_conversion import tensor_sanitize, numpy_sanitize
 from .model_override import guided_relu_policy, deconv_relu_policy, override_relu_gradient, \
                             find_layer
-from .tf_operations import repeat_labels, batch_gradient, batch_predictions_one_hot
+from .tf_operations import repeat_labels, batch_gradient, batch_predictions_one_hot, batch_tensor
