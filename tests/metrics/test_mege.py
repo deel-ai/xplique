cb9e31cfdf20dcb89921037b1e9668a036407ccb
Thomas FEL <thomas.fel@protonmail.com>
2021-09-14 16:39:44 +0200

    mege: make test more robust

--- tests/metrics/test_mege.py
+++ tests/metrics/test_mege.py
@@ -3,20 +3,20 @@ import numpy as np
 
 from ..utils import generate_model, generate_data, almost_equal
 from xplique.metrics import MeGe
-from xplique.attributions import Saliency, DeconvNet
+from xplique.attributions import Saliency, GradientInput
 
 
 def test_best_mege():
     # ensure we get perfect score when the models are the same
-    input_shape, nb_labels, nb_samples = ((4, 4, 1), 10, 20)
+    input_shape, nb_labels, nb_samples = ((8, 8, 1), 10, 80)
     x, y = generate_data(input_shape, nb_labels, nb_samples)
 
     model = generate_model(input_shape, nb_labels)
     learning_algorithm = lambda x_train, y_train, x_test, y_test: model
 
-    for method in [Saliency, DeconvNet]:
+    for method in [Saliency, GradientInput]:
 
-        metric = MeGe(learning_algorithm, x, y, k_splits=2)
+        metric = MeGe(learning_algorithm, x, y, k_splits=4)
         mege, _ = metric.evaluate(method)
 
         assert almost_equal(mege, 1.0)
