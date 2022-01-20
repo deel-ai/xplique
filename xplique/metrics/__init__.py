50fa14043cdd382a0f23eb7902c1a835f0690a4c
DavidPetiteau <86251107+DavidPetiteau@users.noreply.github.com>
2022-01-20 19:09:55 +0100

    Extra functions loading
    File to load the Deletion and Insertion metrics for tabular data

--- xplique/metrics/__init__.py
+++ xplique/metrics/__init__.py
@@ -2,6 +2,6 @@
 Explanations Metrics module
 """
 
-from .fidelity import MuFidelity, Deletion, Insertion, DeletionTS, InsertionTS
+from .fidelity import MuFidelity, Deletion, Insertion, DeletionTS, InsertionTS, Deletion_Tab, Insertion_Tab
 from .stability import AverageStability
 from .representativity import MeGe
