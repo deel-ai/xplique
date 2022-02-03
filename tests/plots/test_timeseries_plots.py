72f46cc52273d983c21a0d7024823d5caedf09cb
Antonin Poche <antonin.poche@irt-saintexupery.com>
2022-02-03 17:11:15 +0100

    test timeseries plots

---
+++ tests/plots/test_timeseries_plots.py
@@ -0,0 +1,57 @@
+import matplotlib
+import numpy as np
+import os
+
+from xplique.plots.timeseries import plot_attributions
+
+
+def test_one_explanation():
+    # test timeseries plot attribution method with one explanation
+    nb_features = 7
+    nb_time_steps = 26
+    title = "test one explanation"
+    filepath = "tests/plots/timeseries_one_attribution_test.png"
+    cmap = "coolwarm"
+
+    features = ["feature_" + str(i) for i in range(nb_features)]
+    explanations = np.random.uniform(size=(nb_features, nb_time_steps))
+
+    plot_attributions(
+        explanations,
+        features,
+        title,
+        filepath,
+        cmap=cmap,
+        colorbar=True,
+    )
+
+    assert os.path.exists(filepath)
+    os.remove(filepath)
+
+
+def test_several_explanations():
+    # test timeseries plot attribution method with several explanations
+    nb_features = 7
+    nb_time_steps = 26
+    title = "test one explanation"
+    filepath = "tests/plots/timeseries_several_attributions_test.png"
+    cmap = "coolwarm"
+
+    features = ["feature_" + str(i) for i in range(nb_features)]
+    explanations = {
+        "method_" + str(i):
+            np.random.uniform(size=(nb_features, nb_time_steps))
+        for i in range(10)
+    }
+
+    plot_attributions(
+        explanations,
+        features,
+        title,
+        filepath,
+        cmap=cmap,
+        colorbar=True,
+    )
+
+    assert os.path.exists(filepath)
+    os.remove(filepath)
