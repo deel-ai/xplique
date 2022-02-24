c6fac7dbe1ece988b0f06ab39d7519e7ab3765a4
Thomas FEL <thomas.fel@protonmail.com>
2022-02-24 19:09:44 +0100

    Bump version: 0.2.6 → 0.3.0

--- xplique/__init__.py
+++ xplique/__init__.py
@@ -6,7 +6,7 @@ The goal of Xplique is to provide a simple interface to the latest explanation
 techniques
 """
 
-__version__ = '0.2.6'
+__version__ = '0.3.0'
 
 from . import attributions
 from . import concepts
