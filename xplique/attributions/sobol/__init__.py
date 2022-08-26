814437bfaedd57efa4cd58be7bfa63e9bedd5f6b
Thomas FEL <thomas_fel@brown.edu>
2022-08-26 12:43:52 +0200

    sobol: introduce global sensitivity module to prepare HSIC attribution method

--- xplique/attributions/sobol/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Sobol Attribution Method
-"""
-from .estimators import (JansenEstimator, JanonEstimator, HommaEstimator, SaltelliEstimator,
-                         GlenEstimator)
-from .perturbations import inpainting, blurring, amplitude
-from .sampling import LHSampler, HaltonSequence, ScipySobolSequence, TFSobolSequence
-from .sobol_attribution_method import SobolAttributionMethod
