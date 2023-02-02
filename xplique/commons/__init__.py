43e1c971127659b4d30ddf82cf214680e3f232bc
Thomas FEL <thomas_fel@brown.edu>
2023-02-02 18:08:44 +0100

    common: introduce exceptions module

--- xplique/commons/__init__.py
+++ xplique/commons/__init__.py
@@ -5,7 +5,9 @@ Utility classes and functions
 from .data_conversion import tensor_sanitize, numpy_sanitize
 from .model_override import guided_relu_policy, deconv_relu_policy, override_relu_gradient, \
                             find_layer, open_relu_policy
-from .tf_operations import repeat_labels, batch_gradient, batch_predictions_one_hot, \
-    batch_tensor, predictions_one_hot, gradient
+from .tf_operations import repeat_labels, batch_tensor
 from .callable_operations import predictions_one_hot_callable, \
     batch_predictions_one_hot_callable
+from .operators import predictions_operator, get_gradient_of_operator, operator_batching, \
+                       batch_predictions, gradients_predictions, batch_gradients_predictions
+from .exceptions import no_gradients_available, raise_invalid_operator
