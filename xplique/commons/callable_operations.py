4af3320f1949cbf56dcf9bf083dbf98822fe979e
Thomas FEL <thomas_fel@brown.edu>
2023-02-02 18:09:50 +0100

    common: cleaning duplicated operators

--- xplique/commons/callable_operations.py
+++ xplique/commons/callable_operations.py
@@ -4,8 +4,8 @@ Custom callable operations
 
 import tensorflow as tf
 
-from .tf_operations import inference_batching
-from ..types import Callable, Optional
+from .operators import operator_batching
+from ..types import Callable
 
 def predictions_one_hot_callable(
     model: Callable,
@@ -56,29 +56,4 @@ def predictions_one_hot_callable(
 
     return scores
 
-
-def batch_predictions_one_hot_callable(model: Callable,
-                              inputs: tf.Tensor,
-                              targets: tf.Tensor,
-                              batch_size: Optional[int] = None) -> tf.Tensor:
-    """
-    Compute predictions scores, only for the label class, for the samples passed. Take
-    care of splitting in multiple batches if batch_size is specified.
-
-    Parameters
-    ----------
-    model
-        Model used for computing predictions score.
-    inputs
-        Input samples to be explained.
-    targets
-        One-hot encoded labels or regression target (e.g {+1, -1}), one for each sample.
-    batch_size
-        Number of samples to predict at once, if None compute all at once.
-
-    Returns
-    -------
-    scores
-        Predictions scores computed, only for the label class.
-    """
-    return inference_batching(predictions_one_hot_callable, model, inputs, targets, batch_size)
+batch_predictions_one_hot_callable = operator_batching(predictions_one_hot_callable)
