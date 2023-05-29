e1e4d82307464be8e462b99e9540f44e298fc6e4
Antonin POCHE <antonin.poche@irt-systemx.fr>
2023-05-29 09:50:25 +0200

    example based: introduce projections

---
+++ xplique/example_based/projections/__init__.py
@@ -0,0 +1,8 @@
+"""
+Projections
+"""
+
+from .attributions import AttributionProjection
+from .base import Projection
+from .custom import CustomProjection
+from .latent_space import LatentSpaceProjection
