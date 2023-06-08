a14091cb60270e8b8674b3f0c112b384fdb16d86
Antonin POCHE <antonin.poche@irt-saintexupery.com>
2023-06-08 12:54:48 +0000

    example based: add image visualization

--- xplique/plots/__init__.py
+++ xplique/plots/__init__.py
@@ -1,5 +1,5 @@
 """
 Utility functions to visualize explanations
 """
-from .image import plot_attributions, plot_attribution
+from .image import plot_attributions, plot_attribution, plot_examples
 from .tabular import plot_feature_impact, plot_mean_feature_impact, summary_plot_tabular
