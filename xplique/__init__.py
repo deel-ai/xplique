7bf23a822489fb2637b0798ac4ea796020dc7d28
lucas Hervier <lucas.hervier@irt-saintexupery.com>
2023-07-18 14:52:37 +0200

    Bump version: 1.0.1 → 1.1.0

--- xplique/__init__.py
+++ xplique/__init__.py
@@ -6,7 +6,7 @@ The goal of Xplique is to provide a simple interface to the latest explanation
 techniques
 """
 
-__version__ = '1.0.3'
+__version__ = '1.1.0'
 
 from . import attributions
 from . import concepts
