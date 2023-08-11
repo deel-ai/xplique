f26e6eb047dac0c58ef5422a5271f1be631240ad
Antonin POCHE <antonin.poche@irt-systemx.fr>
2023-08-11 17:23:13 +0200

    example based: add base class fix up

--- xplique/example_based/similar_examples.py
+++ xplique/example_based/similar_examples.py
@@ -1,34 +1,47 @@
 """
-Base model for example-based 
+Base model for example-based
 """
 
-from abc import ABC, abstractmethod
+import math
 
 import tensorflow as tf
 import numpy as np
 
-from ..types import Callable, Dict, List, Optional, Tuple, Type, Union
+from ..types import Callable, Dict, List, Optional, Type, Union
 
 from ..commons import sanitize_inputs_targets
-from .search_methods import SklearnKNN
-from .search_methods.base import BaseSearchMethod
-from .projections.base import Projection
+from ..commons import sanitize_dataset, dataset_gather
+from .search_methods import KNN, BaseSearchMethod
+from .projections import Projection
+
+from .search_methods.base import _sanitize_returns
 
 
-class SimilarExamples():
+class SimilarExamples:
     """
     Base class for natural example-base methods explaining models,
-    they project the case_dataset into a pertinent space for the with a `Projection`,
+    they project the cases_dataset into a pertinent space for the with a `Projection`,
     then they call the `BaseSearchMethod` on it.
 
     Parameters
     ----------
-    case_dataset
+    cases_dataset
         The dataset used to train the model, examples are extracted from the dataset.
-    dataset_labels
-        Labels associated to the examples in the dataset. Indices should match with case_dataset.
-    dataset_targets
-        Targets associated to the case_dataset for dataset projection. See `projection` for detail.
+        `tf.data.Dataset` are assumed to be batched as tensorflow provide no method to verify it.
+        Becareful, `tf.data.Dataset` are often reshuffled at each iteration, be sure that it is not
+        the case for your dataset, otherwise, examples will not make sense.
+    labels_dataset
+        Labels associated to the examples in the dataset. Indices should match with cases_dataset.
+        `tf.data.Dataset` are assumed to be batched as tensorflow provide no method to verify it.
+        Batch size and cardinality of other dataset should match `cases_dataset`.
+        Becareful, `tf.data.Dataset` are often reshuffled at each iteration, be sure that it is not
+        the case for your dataset, otherwise, examples will not make sense.
+    targets_dataset
+        Targets associated to the cases_dataset for dataset projection. See `projection` for detail.
+        `tf.data.Dataset` are assumed to be batched as tensorflow provide no method to verify it.
+        Batch size and cardinality of other dataset should match `cases_dataset`.
+        Becareful, `tf.data.Dataset` are often reshuffled at each iteration, be sure that it is not
+        the case for your dataset, otherwise, examples will not make sense.
     search_method
         An algorithm to search the examples in the projected space.
     k
@@ -53,45 +66,145 @@ class SimilarExamples():
     case_returns
         String or list of string with the elements to return in `self.explain()`.
         See `self.set_returns()` for detail.
+    batch_size
+        Number of sample treated simultaneously for projection and search.
+        Ignored if `tf.data.Dataset` are provided (those are supposed to be batched).
     search_method_kwargs
         Parameters to be passed at the construction of the `search_method`.
     """
 
-    def __init__(self,
-                 case_dataset: Union[tf.data.Dataset, tf.Tensor, np.ndarray],
-                 labels_dataset: Union[tf.Tensor, np.ndarray] = None,
-                 dataset_targets: Union[tf.Tensor, np.ndarray] = None,
-                 search_method: Type[BaseSearchMethod] = SklearnKNN,
-                 k: int = 1,
-                 projection: Union[Projection, Callable] = None,
-                 case_returns: Union[List[str], str] = "examples",
-                 **search_method_kwargs):
-        assert projection is not None,\
-            "`SimilarExamples` without `projection` is a `BaseSearchMethod`."
+    def __init__(
+        self,
+        cases_dataset: Union[tf.data.Dataset, tf.Tensor, np.ndarray],
+        labels_dataset: Optional[Union[tf.data.Dataset, tf.Tensor, np.ndarray]] = None,
+        targets_dataset: Optional[Union[tf.data.Dataset, tf.Tensor, np.ndarray]] = None,
+        search_method: Type[BaseSearchMethod] = KNN,
+        k: int = 1,
+        projection: Union[Projection, Callable] = None,
+        case_returns: Union[List[str], str] = "examples",
+        batch_size: Optional[int] = 32,
+        **search_method_kwargs,
+    ):
+        assert (
+            projection is not None
+        ), "`SimilarExamples` without `projection` is a `BaseSearchMethod`."
 
         # set attributes
-        if isinstance(case_dataset, tuple) and labels_dataset is None:
-            # assuming (x_train, y_train)
-            self.case_dataset = case_dataset[0]
-            self.labels_dataset = case_dataset[1]
-        else:
-            self.case_dataset = case_dataset
-            self.labels_dataset = labels_dataset
-        self.dataset_targets = dataset_targets
+        batch_size = self.__initialize_cases_dataset(
+            cases_dataset, labels_dataset, targets_dataset, batch_size
+        )
         self.k = k
         self.set_returns(case_returns)
         self.projection = projection
 
-        # project dataset
-        projected_dataset = self.projection(self.case_dataset, self.dataset_targets)
-
         # set `search_returns` if not provided and overwrite it otherwise
         search_method_kwargs["search_returns"] = ["indices", "distances"]
-                
 
         # initiate search_method
-        self.search_method = search_method(search_set=projected_dataset, 
-                                           k=k, **search_method_kwargs)
+        self.search_method = search_method(
+            cases_dataset=cases_dataset,
+            targets_dataset=targets_dataset,
+            k=k,
+            projection=projection,
+            batch_size=batch_size,
+            **search_method_kwargs,
+        )
+
+    def __initialize_cases_dataset(
+        self,
+        cases_dataset: Union[tf.data.Dataset, tf.Tensor, np.ndarray],
+        labels_dataset: Optional[Union[tf.data.Dataset, tf.Tensor, np.ndarray]],
+        targets_dataset: Optional[Union[tf.data.Dataset, tf.Tensor, np.ndarray]],
+        batch_size: Optional[int],
+    ) -> int:
+        """
+        Factorization of `__init__()` method for dataset related attributes.
+
+        Parameters
+        ----------
+        cases_dataset
+            The dataset used to train the model, examples are extracted from the dataset.
+        labels_dataset
+            Labels associated to the examples in the dataset.
+            Indices should match with cases_dataset.
+        targets_dataset
+            Targets associated to the cases_dataset for dataset projection.
+            See `projection` for detail.
+        batch_size
+            Number of sample treated simultaneously when using the datasets.
+            Ignored if `tf.data.Dataset` are provided (those are supposed to be batched).
+
+        Returns
+        -------
+        batch_size
+            Number of sample treated simultaneously when using the datasets.
+            Extracted from the datasets in case they are `tf.data.Dataset`.
+            Otherwise, the input value.
+        """
+        # at least one dataset provided
+        if isinstance(cases_dataset, tf.data.Dataset):
+            # set batch size (ignore provided argument) and cardinality
+            if isinstance(cases_dataset.element_spec, tuple):
+                batch_size = tf.shape(next(iter(cases_dataset))[0])[0].numpy()
+            else:
+                batch_size = tf.shape(next(iter(cases_dataset)))[0].numpy()
+
+            cardinality = cases_dataset.cardinality().numpy()
+        else:
+            # if case_dataset is not a `tf.data.Dataset`, then neither should the other.
+            assert not isinstance(labels_dataset, tf.data.Dataset)
+            assert not isinstance(targets_dataset, tf.data.Dataset)
+            # set batch size and cardinality
+            batch_size = min(batch_size, len(cases_dataset))
+            cardinality = math.ceil(len(cases_dataset) / batch_size)
+
+        # verify cardinality and create datasets from the tensors
+        self.cases_dataset = sanitize_dataset(
+            cases_dataset, batch_size, cardinality
+        )
+        self.labels_dataset = sanitize_dataset(
+            labels_dataset, batch_size, cardinality
+        )
+        self.targets_dataset = sanitize_dataset(
+            targets_dataset, batch_size, cardinality
+        )
+
+        # if the provided `cases_dataset` has several columns
+        if isinstance(self.cases_dataset.element_spec, tuple):
+            # switch case on the number of columns of `cases_dataset`
+            if len(self.cases_dataset.element_spec) == 2:
+                assert self.labels_dataset is None, (
+                    "The second column of `cases_dataset` is assumed to be the labels."
+                    + "Hence, `labels_dataset` should be empty."
+                )
+                self.labels_dataset = self.cases_dataset.map(lambda x, y: y)
+                self.cases_dataset = self.cases_dataset.map(lambda x, y: x)
+
+            elif len(self.cases_dataset.element_spec) == 3:
+                assert self.labels_dataset is None, (
+                    "The second column of `cases_dataset` is assumed to be the labels."
+                    + "Hence, `labels_dataset` should be empty."
+                )
+                assert self.targets_dataset is None, (
+                    "The second column of `cases_dataset` is assumed to be the labels."
+                    + "Hence, `labels_dataset` should be empty."
+                )
+                self.targets_dataset = self.cases_dataset.map(lambda x, y, t: t)
+                self.labels_dataset = self.cases_dataset.map(lambda x, y, t: y)
+                self.cases_dataset = self.cases_dataset.map(lambda x, y, t: x)
+            else:
+                raise AttributeError(
+                    "`cases_dataset` cannot possess more than 3 columns,"
+                    + f"{len(self.cases_dataset.element_spec)} were detected."
+                )
+
+        self.cases_dataset = self.cases_dataset.prefetch(tf.data.AUTOTUNE)
+        if self.labels_dataset is not None:
+            self.labels_dataset = self.labels_dataset.prefetch(tf.data.AUTOTUNE)
+        if self.targets_dataset is not None:
+            self.targets_dataset = self.targets_dataset.prefetch(tf.data.AUTOTUNE)
+
+        return batch_size
 
     def set_k(self, k: int):
         """
@@ -109,7 +222,7 @@ class SimilarExamples():
     def set_returns(self, returns: Union[List[str], str]):
         """
         Set `self.returns` used to define returned elements in `self.explain()`.
-        
+
         Parameters
         ----------
         returns
@@ -126,41 +239,30 @@ class SimilarExamples():
                 - 'include_inputs' specify if inputs should be included in the returned elements.
                 Note that it changes the number of returned elements from k to k+1.
         """
-        if returns is None:
-            self.returns = ["examples"]
-        elif isinstance(returns, str):
-            possibilities = ["examples", "weights", "distances", "labels", "include_inputs"]
-            if returns == "all":
-                self.returns = possibilities
-            elif returns in possibilities:
-                self.returns = [returns]
-            else:
-                raise ValueError(f"{returns} should belong to {possibilities}")
-        elif isinstance(returns, list):
-            self.returns = returns
-        else:
-            raise ValueError(f"{returns} should either be `str` or `List[str]`")
-        
+        possibilities = ["examples", "weights", "distances", "labels", "include_inputs"]
+        default = "examples"
+        self.returns = _sanitize_returns(returns, possibilities, default)
+
     @sanitize_inputs_targets
-    def explain(self,
-                inputs: Union[tf.data.Dataset, tf.Tensor, np.ndarray],
-                targets: Optional[Union[tf.Tensor, np.ndarray]] = None):
+    def explain(
+        self,
+        inputs: Union[tf.Tensor, np.ndarray],
+        targets: Optional[Union[tf.Tensor, np.ndarray]] = None,
+    ):
         """
         Compute examples to explain the inputs.
         It project inputs with `self.projection` in the search space
         and find examples with `self.search_method`.
-        
+
+        Parameters
+        ----------
         inputs
-            Dataset, Tensor or Array. Input samples to be explained.
-            If Dataset, targets should not be provided (included in Dataset).
+            Tensor or Array. Input samples to be explained.
             Expected shape among (N, W), (N, T, W), (N, W, H, C).
             More information in the documentation.
         targets
             Tensor or Array passed to the projection function.
-            Here it is used by the explain function of attribution methods.
-            Refer to the corresponding method documentation for more detail.
-            Note that the default method is `Saliency`.
-            
+
         Returns
         -------
         return_dict
@@ -178,26 +280,29 @@ class SimilarExamples():
         # manage returned elements
         return self.format_search_output(search_output, inputs, targets)
 
-    def __call__(self,
-                 inputs: Union[tf.data.Dataset, tf.Tensor, np.ndarray],
-                 targets: Optional[Union[tf.Tensor, np.ndarray]] = None):
+    def __call__(
+        self,
+        inputs: Union[tf.Tensor, np.ndarray],
+        targets: Optional[Union[tf.Tensor, np.ndarray]] = None,
+    ):
         """explain alias"""
         return self.explain(inputs, targets)
-    
-    def format_search_output(self,
-                             search_output: Dict[str, tf.Tensor],
-                             inputs: Union[tf.data.Dataset, tf.Tensor, np.ndarray],
-                             targets: Optional[Union[tf.Tensor, np.ndarray]] = None):
+
+    def format_search_output(
+        self,
+        search_output: Dict[str, tf.Tensor],
+        inputs: Union[tf.Tensor, np.ndarray],
+        targets: Optional[Union[tf.Tensor, np.ndarray]] = None,
+    ):
         """
         Format the output of the `search_method` to match the expected returns in `self.returns`.
-        
+
         Parameters
         ----------
         search_output
-            Dictionnary with the required outputs from the `search_method`. 
+            Dictionnary with the required outputs from the `search_method`.
         inputs
-            Dataset, Tensor or Array. Input samples to be explained.
-            If Dataset, targets should not be provided (included in Dataset).
+            Tensor or Array. Input samples to be explained.
             Expected shape among (N, W), (N, T, W), (N, W, H, C).
             More information in the documentation.
         targets
@@ -205,7 +310,7 @@ class SimilarExamples():
             Here it is used by the explain function of attribution methods.
             Refer to the corresponding method documentation for more detail.
             Note that the default method is `Saliency`.
-            
+
         Returns
         -------
         return_dict
@@ -216,12 +321,14 @@ class SimilarExamples():
         """
         return_dict = {}
 
+        examples = dataset_gather(self.cases_dataset, search_output["indices"])
+        examples_labels = dataset_gather(self.labels_dataset, search_output["indices"])
+        examples_targets = dataset_gather(
+            self.targets_dataset, search_output["indices"]
+        )
+
         # add examples and weights
-        if "examples" or "weights" in self.returns:
-            # get examples from indices
-            examples = tf.gather(self.case_dataset, search_output["indices"])
-            if targets is not None:
-                examples_targets = tf.gather(self.dataset_targets, search_output["indices"])
+        if "examples" in self.returns or "weights" in self.returns:
             if "include_inputs" in self.returns:
                 # include inputs
                 inputs = tf.expand_dims(inputs, axis=1)
@@ -235,7 +342,7 @@ class SimilarExamples():
                 return_dict["examples"] = examples
             if "weights" in self.returns:
                 # get weights of examples (n, k, ...)
-                # we iterate on the inputs dimension through maps 
+                # we iterate on the inputs dimension through maps
                 # and ask weights for batch of examples
                 weights = []
                 for ex, ex_targ in zip(examples, examples_targets):
@@ -244,28 +351,30 @@ class SimilarExamples():
                         weights.append(self.projection.get_input_weights(ex, ex_targ))
                     else:
                         raise AttributeError(
-                            "Cannot extract weights from the provided projection function"+\
-                            "Either remove 'weights' from the `case_returns` or"+\
-                            "inherit from `Projection` and overwrite `get_input_weights`.")
-                    
+                            "Cannot extract weights from the provided projection function"
+                            + "Either remove 'weights' from the `case_returns` or"
+                            + "inherit from `Projection` and overwrite `get_input_weights`."
+                        )
+
                 return_dict["weights"] = tf.stack(weights, axis=0)
 
-                # optimization test
+                # optimization test TODO
                 # return_dict["weights"] = tf.vectorized_map(
                 #     fn=lambda x: self.projection.get_input_weights(x[0], x[1]),
                 #     elems=(examples, examples_targets),
                 #     # fn_output_signature=tf.float32,
                 # )
-        
+
         # add indices, distances, and labels
         if "distances" in self.returns:
             return_dict["distances"] = search_output["distances"]
         if "labels" in self.returns:
-            assert self.labels_dataset is not None, "Cole method cannot return labels without a label dataset."
-            return_dict["labels"] = tf.gather(self.labels_dataset, search_output["indices"])
-        
+            assert (
+                examples_labels is not None
+            ), "The method cannot return labels without a label dataset."
+            return_dict["labels"] = examples_labels
+
         # return a dict only different variables are returned
         if len(return_dict) == 1:
             return list(return_dict.values())[0]
-        else:
-            return return_dict
+        return return_dict
