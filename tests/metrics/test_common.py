6c7e2765ffa817c5493a8341b764c2d56f0c61ef
Antonin POCHE <antonin.poche@irt-systemx.fr>
2023-10-12 16:15:42 +0200

    tests metrics: ensure different data types formats are supported

--- tests/metrics/test_common.py
+++ tests/metrics/test_common.py
@@ -6,7 +6,7 @@ from xplique.attributions import (Saliency, GradientInput, IntegratedGradients, 
                                   GradCAMPP)
 from xplique.metrics import MuFidelity, Deletion, Insertion, AverageStability
 from xplique.metrics.base import ExplanationMetric, ExplainerMetric
-from ..utils import generate_data, generate_model
+from ..utils import generate_data, generate_model, generate_regression_model
 
 
 def _default_methods(model, output_layer_index=-2):
@@ -95,3 +95,45 @@ def test_add_activation():
                         assert np.all(score <= 1)
                         assert np.all(score >= 0)
                 assert type(score) in [np.float32, np.float64, float]
+
+def test_data_types_shapes():
+    """Test that all the attributions method works as explainer"""
+
+    data_types_input_shapes = {
+        "tabular": (20,),
+        "time-series": (20, 10),
+        "images rgb": (20, 16, 3),
+        "images black and white": (28, 28, 1),
+    }
+
+    explainer_metrics = {
+        AverageStability: {"nb_samples": 3}
+    }
+    explanation_metrics = {
+        Deletion: {"steps": 3},
+        Insertion: {"steps": 3},
+        MuFidelity: {"nb_samples": 3},
+    }
+
+    for data_type, input_shape in data_types_input_shapes.items():
+        input_shape, nb_labels, samples = (input_shape, 5, 15)
+        inputs, targets = generate_data(input_shape, nb_labels, samples)
+
+        if len(input_shape) == 3:  # image => conv2D
+            model = generate_model(input_shape, nb_labels)
+            model.layers[-1].activation = tf.keras.activations.linear
+        else:  # others => dense
+            model = generate_regression_model(input_shape, nb_labels)
+
+        explainer = Saliency(model)
+        explanation = explainer(inputs, targets)
+
+        for metric_class, params in explanation_metrics.items():
+            metric = metric_class(model, inputs, targets, **params)
+            score = metric(explanation)
+            assert type(score) in [np.float32, np.float64, float]
+
+        for metric_class, params in explainer_metrics.items():
+            metric = metric_class(model, inputs, targets, **params)
+            score = metric(explainer)
+            assert type(score) in [np.float32, np.float64, float]
