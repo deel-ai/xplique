aba78f98b9092106c80fbd9dbc3e1ca23b115bb6
Antonin POCHE <antonin.poche@irt-systemx.fr>
2023-10-13 15:47:38 +0200

    test hsic: test introduced estimator batch size

--- tests/attributions/test_hsic.py
+++ tests/attributions/test_hsic.py
@@ -44,3 +44,22 @@ def test_estimators():
             stis_maps = method.explain(x, y)
 
             assert x.shape[:-1] == stis_maps.shape[:-1]
+
+def test_estimator_batch_size():
+    """Ensure estimator batch size is correctly used"""
+    input_shape = (20, 20, 1)
+    nb_labels = 10
+    x, y = generate_data(input_shape, nb_labels, 10)
+    model = generate_model(input_shape, nb_labels)
+
+    for estimator_batch_size in [None, 1, 4, 7, 23, 37, 103]:
+        method = HsicAttributionMethod(
+            model, grid_size=5, nb_design=10, estimator_batch_size=estimator_batch_size
+        )
+        if estimator_batch_size is None:
+            assert method.estimator.batch_size == 100000
+        else:
+            assert method.estimator.batch_size == estimator_batch_size
+        
+        stis_maps = method.explain(x, y)
+        assert x.shape[:-1] == stis_maps.shape[:-1]
