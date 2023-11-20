4f97b734ab77245c639b3c704f3e21260ef4cdb7
Mohamed Chafik Bakey <mohamed-chafik.bakey@irt-saintexupery.com>
2023-11-20 15:06:37 +0100

    add MMD-critic method fix up

--- xplique/example_based/search_methods/mmd_critic.py
+++ xplique/example_based/search_methods/mmd_critic.py
@@ -18,21 +18,95 @@ from .protogreedy import Protogreedy
 
 
 class MMDCritic(Protogreedy):
-    
+    """
+    MMDCritic method to search prototypes.
 
+    References:
+    .. [#] `Been Kim, Rajiv Khanna, Oluwasanmi Koyejo, 
+        "Examples are not enough, learn to criticize! criticism for interpretability"
+        <https://proceedings.neurips.cc/paper_files/paper/2016/file/5680522b8e2bb01943234bce7bf84534-Paper.pdf>`_
+    
+    
+    Parameters
+    ----------
+    cases_dataset : Union[tf.data.Dataset, tf.Tensor, np.ndarray]
+        The dataset used to train the model, examples are extracted from the dataset.
+        For natural example-based methods, it is the train dataset.
+    labels_dataset : Optional[Union[tf.data.Dataset, tf.Tensor, np.ndarray]]
+        Targets associated with the cases_dataset for dataset projection.
+        See `projection` for details.
+    targets_dataset : Union[tf.data.Dataset, tf.Tensor, np.ndarray]
+        The dataset used for projecting samples from the input space to the search space.
+        The search space should be a space where distances make sense for the model.
+        It should not be `None`, otherwise, all examples could be computed only with the `search_method`.
+    k : int, optional
+        The number of examples to retrieve, by default 1.
+    projection : Union[Projection, Callable], optional
+        Projection or Callable that projects samples from the input space to the search space.
+        The search space should be a space where distance makes sense for the model.
+        It should not be `None`, otherwise, all examples could be computed only with the `search_method`.
+        Example of Callable:
+        ```
+        def custom_projection(inputs: tf.Tensor, np.ndarray, targets: tf.Tensor, np.ndarray = None):
+            '''
+            Example of projection,
+            inputs are the elements to project.
+            targets are optional parameters to orientate the projection.
+            '''
+            projected_inputs = # do some magic on inputs, it should use the model.
+            return projected_inputs
+        ```
+    search_returns : Optional[Union[List[str], str]], optional
+        String or list of strings with the elements to return in `self.find_examples()`.
+        See `self.set_returns()` for details, by default None.
+    batch_size : Optional[int], optional
+        Number of samples treated simultaneously.
+        It should match the batch size of the `search_set` in the case of a `tf.data.Dataset`, by default 32.
+    distance : Union[int, str, Callable], optional
+        Either a Callable, or a value supported by `tf.norm` `ord` parameter.
+        Supported values are 'fro', 'euclidean', 'cosine', 1, 2, np.inf,
+        and any positive real number yielding the corresponding p-norm, by default "euclidean".
+    kernel : Union[Callable, tf.Tensor, np.ndarray], optional
+        Kernel function or kernel matrix, by default rbf_kernel.
+    kernel_type : str, optional
+        The kernel type. It can be 'local' or 'global', by default 'local'.
+        When it is local, the distances are calculated only within the classes.
+    use_optimiser : bool, optional
+        Flag indicating whether to use an optimizer for prototype selection, by default False.
+    """
+    
     def compute_objective(self, S, Sw, c):
-        
-        # special case of protogreedy where we give equal weights to all prototypes, 
-        # the objective here is simplified to speed up processing
         """
+        Compute the objective function and corresponding weights for a given set of selected prototypes and a candidate.
+
+        Here, we have a special case of protogreedy where we give equal weights to all prototypes, 
+        the objective here is simplified to speed up processing
+     
         Find argmax_{c} F(S ∪ c) - F(S)
         ≡
         Find argmax_{c} F(S ∪ c)
         ≡
         Find argmax_{c} (sum1 - sum2) where: sum1 = (2 / n) * ∑[i=1 to n] κ(x_i, c) 
                                                 sum2 = 1/(|S|+1) [2 * ∑[j=1 to |S|] * κ(x_j, c) + κ(c, c)]
-        """
+        
+        Parameters
+        ----------
+        S : Tensor
+            Indices of currently selected prototypes.
+        Sw : Tensor
+            Weights corresponding to the selected prototypes.
+        c : Tensor
+            Indices of the candidate prototype to be considered.
 
+        Returns
+        -------
+        Tuple
+            A tuple containing:
+            - objective : Tensor
+                The computed objective function value.
+            - objective_weights : Tensor
+                The weights corresponding to the optimal solution of the objective function.
+        """        
         sum1 = 2 * tf.gather(self.colmean, c)
 
         if S.shape[0] == 0:
@@ -46,9 +120,21 @@ class MMDCritic(Protogreedy):
         objective_weights = tf.ones(shape=(c.shape[0], S.shape[0]+1), dtype=tf.float32) / tf.cast(S.shape[0]+1, dtype=tf.float32)
 
         return objective, objective_weights
-    
-    
+     
     def compute_MMD_distance(self, Z):
+        """
+        Compute the Maximum Mean Discrepancy (MMD) distance for a set of prototypes.
+
+        Parameters
+        ----------
+        Z : Tensor
+            Indices of the selected prototypes.
+
+        Returns
+        -------
+        Tensor
+            The computed MMD distance for the given set of prototypes.
+        """
 
         Zw = tf.ones_like(Z, dtype=tf.float32) / tf.cast(Z.shape[0], dtype=tf.float32)
 
--- xplique/example_based/search_methods/protogreedy.py
+++ xplique/example_based/search_methods/protogreedy.py
@@ -16,6 +16,21 @@ from .base import BaseSearchMethod
 from ..projections import Projection
 
 class Optimiser():
+    """
+    Class to solve the quadratic problem:
+    F(S) ≡ max_{w:supp(w)∈ S, w ≥ 0} l(w), 
+    where l(w) = w^T * μ_p - 1/2 * w^T * K * w
+
+    Parameters
+    ----------
+    initial_w
+        Initial weight.
+    minWeight
+        Lower bound on weight.
+    maxWeight
+        Upper bound on weight.
+
+    """
 
     def __init__(
             self, 
@@ -30,15 +45,35 @@ class Optimiser():
         self.objective = self.create_objective()
 
     def create_objective(self):
+        """
+        Create the objective function to be optimized.
+
+        Returns:
+        The objective function, which is defined as the negative of l(w) to maximize.
+        """
         def objective(w, u, K):
             return - (w @ u - 0.5 * w @ K @ w)  # Negative of l(w) to maximize
         return objective
 
     def optimize(self, u, K):
-        """"
-        Perform the optimization
-        F(S) ≡ max_{w:supp(w)∈ S, w ≥ 0} l(w), 
-        where l(w) = w^T * μ_p - 1/2 * w^T * K * w
+        """
+        Perform optimization to find the optimal values of the weight vector (w) and the corresponding objective function value.
+
+        Parameters
+        ----------
+        u : Tensor
+            Mean similarity of each prototype.
+        K : Tensor
+            The kernel matrix.
+
+        Returns
+        -------
+        Tuple
+            A tuple containing:
+            - optimal_w : Tensor
+                The optimal value of the weight vector (w).
+            - F_S : Tensor
+                The value of the objective function corresponding to the optimal_w.
         """
         u = u.numpy()
         K = K.numpy()
@@ -67,7 +102,7 @@ class Optimiser():
 
 class Protogreedy(BaseSearchMethod):
     """
-    Protogreedy method to search prototypes.
+    Protogreedy method for searching prototypes.
 
     References:
     .. [#] `Karthik S. Gurumoorthy, Amit Dhurandhar, Guillermo Cecchi,
@@ -76,41 +111,50 @@ class Protogreedy(BaseSearchMethod):
 
     Parameters
     ----------
-    cases_dataset
+    cases_dataset : Union[tf.data.Dataset, tf.Tensor, np.ndarray]
         The dataset used to train the model, examples are extracted from the dataset.
-        For natural example-based methods it is the train dataset.
-    targets_dataset
-        Targets associated to the cases_dataset for dataset projection. See `projection` for detail.
-    k
-        The number of examples to retrieve.
-    projection
-        Projection or Callable that project samples from the input space to the search space.
-        The search space sould be a space where distance make sense for the model.
-        It should not be `None`, otherwise,
-        all examples could be computed only with the `search_method`.
-
+        For natural example-based methods, it is the train dataset.
+    labels_dataset : Optional[Union[tf.data.Dataset, tf.Tensor, np.ndarray]]
+        Targets associated with the cases_dataset for dataset projection.
+        See `projection` for details.
+    targets_dataset : Union[tf.data.Dataset, tf.Tensor, np.ndarray]
+        The dataset used for projecting samples from the input space to the search space.
+        The search space should be a space where distances make sense for the model.
+        It should not be `None`, otherwise, all examples could be computed only with the `search_method`.
+    k : int, optional
+        The number of examples to retrieve, by default 1.
+    projection : Union[Projection, Callable], optional
+        Projection or Callable that projects samples from the input space to the search space.
+        The search space should be a space where distance makes sense for the model.
+        It should not be `None`, otherwise, all examples could be computed only with the `search_method`.
         Example of Callable:
         ```
         def custom_projection(inputs: tf.Tensor, np.ndarray, targets: tf.Tensor, np.ndarray = None):
             '''
             Example of projection,
             inputs are the elements to project.
-            targets are optionnal parameters to orientated the projection.
+            targets are optional parameters to orientate the projection.
             '''
             projected_inputs = # do some magic on inputs, it should use the model.
             return projected_inputs
         ```
-    search_returns
-        String or list of string with the elements to return in `self.find_examples()`.
-        See `self.set_returns()` for detail.
-    batch_size
-        Number of sample treated simultaneously.
-        It should match the batch size of the `search_set` in the case of a `tf.data.Dataset`.
-    distance
+    search_returns : Optional[Union[List[str], str]], optional
+        String or list of strings with the elements to return in `self.find_examples()`.
+        See `self.set_returns()` for details, by default None.
+    batch_size : Optional[int], optional
+        Number of samples treated simultaneously.
+        It should match the batch size of the `search_set` in the case of a `tf.data.Dataset`, by default 32.
+    distance : Union[int, str, Callable], optional
         Either a Callable, or a value supported by `tf.norm` `ord` parameter.
-        Their documentation (https://www.tensorflow.org/api_docs/python/tf/norm) say:
-        "Supported values are 'fro', 'euclidean', 1, 2, np.inf and any positive real number
-        yielding the corresponding p-norm." We also added 'cosine'.
+        Supported values are 'fro', 'euclidean', 'cosine', 1, 2, np.inf,
+        and any positive real number yielding the corresponding p-norm, by default "euclidean".
+    kernel : Union[Callable, tf.Tensor, np.ndarray], optional
+        Kernel function or kernel matrix, by default rbf_kernel.
+    kernel_type : str, optional
+        The kernel type. It can be 'local' or 'global', by default 'local'.
+        When it is local, the distances are calculated only within the classes.
+    use_optimiser : bool, optional
+        Flag indicating whether to use an optimizer for prototype selection, by default False.
     """
 
     def __init__(
@@ -125,6 +169,7 @@ class Protogreedy(BaseSearchMethod):
         distance: Union[int, str, Callable] = "euclidean",
         kernel: Union[Callable, tf.Tensor, np.ndarray] = rbf_kernel,
         kernel_type: str = 'local',
+        use_optimiser: bool = False,
     ): # pylint: disable=R0801
         super().__init__(
             cases_dataset, labels_dataset, targets_dataset, k, projection, search_returns, batch_size
@@ -170,15 +215,34 @@ class Protogreedy(BaseSearchMethod):
             if distance is None:
                 distance = "euclidean"
 
-        self.use_optimiser = False
+        self.use_optimiser = use_optimiser
         self.sample_indices = tf.range(0, self.kernel_matrix.shape[0])
         self.n = self.sample_indices.shape[0]
         self.colsum = tf.reduce_sum(self.kernel_matrix, axis=0)
         self.colmean = self.colsum / self.n 
 
-
     @staticmethod
     def compute_kernel_matrix(X, y, kernel, kernel_type):
+        """
+        Compute the kernel matrix to be used for finding prototypes.
+
+        Parameters
+        ----------
+        X : Tensor
+            Input data.
+        y : Tensor
+            Labels corresponding to the input data.
+        kernel : Callable
+            Function that computes the distance between two arrays.
+        kernel_type : str
+            The kernel type. It can be 'local' or 'global'.
+            When it is local, the distances are calculated only within the classes.
+
+        Returns
+        -------
+        Tensor
+            The computed kernel matrix based on the specified kernel and kernel type.
+        """
     
         if kernel_type == 'local':
             # only calculate distance within class. across class, distance = 0
@@ -192,17 +256,32 @@ class Protogreedy(BaseSearchMethod):
         
         elif kernel_type == 'global':
             kernel_matrix = kernel(X)
+        
+        kernel_matrix = tf.cast(kernel_matrix, dtype=tf.float32)
 
         return kernel_matrix
     
-
     def compute_weighted_MMD_distance(self, Z, Zw):
         """
-        weighted_MMD2 = (1/n**2) * ∑(i, j=1 to n) k(xi, xj)
-                    - (2/n) * ∑(j=1 to m) Zw_j * ∑(i=1 to n) k(xi, zj)
-                    + ∑(i, j=1 to m) Zw_i * Zw_j * k(zi, zj)
-        """
+        Compute the weighted Maximum Mean Discrepancy (MMD) distance.
 
+        The weighted MMD^2 is calculated using the formula:
+            weighted_MMD2 = (1/n**2) * ∑(i, j=1 to n) k(xi, xj)
+                            - (2/n) * ∑(j=1 to m) Zw_j * ∑(i=1 to n) k(xi, zj)
+                            + ∑(i, j=1 to m) Zw_i * Zw_j * k(zi, zj)
+
+        Parameters
+        ----------
+        Z : Tensor
+            Indices of selected prototypes.
+        Zw : Tensor
+            Weights corresponding to the selected prototypes.
+
+        Returns
+        -------
+        Tensor
+            The computed weighted MMD distance.
+        """
         temp = tf.gather(tf.gather(self.kernel_matrix, Z), Z, axis=1)
         temp = temp * tf.expand_dims(Zw, axis=1)
         temp = temp * tf.expand_dims(Zw, axis=0)
@@ -217,13 +296,33 @@ class Protogreedy(BaseSearchMethod):
     
     
     def compute_objective(self, S, Sw, c):
+        """
+        Compute the objective function and corresponding weights for a given set of selected prototypes and a candidate.
 
+        Parameters
+        ----------
+        S : Tensor
+            Indices of currently selected prototypes.
+        Sw : Tensor
+            Weights corresponding to the selected prototypes.
+        c : Tensor
+            Indices of the candidate prototype to be considered.
+
+        Returns
+        -------
+        Tuple
+            A tuple containing:
+            - objective : Tensor
+                The computed objective function value.
+            - objective_weights : Tensor
+                The weights corresponding to the optimal solution of the objective function.
+        """
         # Concatenate selection with each candidate
         # S = S ∪ {c}
         repeated_selected_indices = tf.tile(tf.expand_dims(S, 0), [c.shape[0], 1])
         all_indices = tf.concat([repeated_selected_indices, tf.expand_dims(c, 1)], axis=1)
 
-        if (self.use_optimiser):
+        if self.use_optimiser:
 
             opt = Optimiser(initial_w=tf.concat([Sw, [0]], axis=0))
             
@@ -255,7 +354,7 @@ class Protogreedy(BaseSearchMethod):
             # determinant = tf.linalg.det(K)
             # is_not_invertible = tf.math.abs(determinant) <= 1e-6
 
-            # We added epsilon to the diagonal of K to ensure that K is invertible*          
+            # We added epsilon to the diagonal of K to ensure that K is invertible          
             epsilon = 1e-6
             K_inv = tf.linalg.inv(K + epsilon * tf.eye(K.shape[-1]))
 
@@ -269,9 +368,34 @@ class Protogreedy(BaseSearchMethod):
 
         return objective, objective_weights
     
-
     def update_selection(self, selected_indices, selected_weights, objective, objective_weights, objective_argmax, best_sample_index):
+        """
+        Update the set of selected prototypes and their corresponding weights based on the optimization results.
 
+        Parameters
+        ----------
+        selected_indices : Tensor
+            Indices of currently selected prototypes.
+        selected_weights : Tensor
+            Weights corresponding to the selected prototypes.
+        objective : Tensor
+            The computed objective function values for each candidate.
+        objective_weights : Tensor
+            The weights corresponding to the optimal solution of the objective function for each candidate.
+        objective_argmax : Tensor
+            The index of the candidate with the highest objective function value.
+        best_sample_index : Tensor
+            The index of the selected prototype with the highest objective function value.
+
+        Returns
+        -------
+        Tuple
+            A tuple containing:
+            - selected_indices : Tensor
+                Updated indices of selected prototypes.
+            - selected_weights : Tensor
+                Updated weights corresponding to the selected prototypes.
+        """
         # update selected_indices 
         selected_indices = tf.concat([selected_indices, [best_sample_index]], axis=0)
 
@@ -280,8 +404,20 @@ class Protogreedy(BaseSearchMethod):
        
         return selected_indices, selected_weights
         
-
     def find_prototypes(self, num_prototypes):
+        """
+        Search for prototypes and their corresponding weights using an iterative selection process.
+
+        Parameters
+        ----------
+        num_prototypes : int
+            Number of prototypes to find.
+
+        Returns
+        -------
+        None
+            The function updates the object's state with the selected prototype indices and their normalized weights.
+        """
         # Initialize a binary mask to keep track of selected samples.
         is_selected = tf.zeros_like(self.sample_indices)
         # Initialize empty lists to store selected indices and their corresponding weights.
