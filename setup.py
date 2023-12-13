faced853d7ffe22242726fe12da25806860ea366
Antonin POCHE <antonin.poche@irt-systemx.fr>
2023-12-13 16:11:49 +0100

    Bump version: 1.3.2 → 1.3.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as fh:
 
 setup(
     name="Xplique",
-    version="1.3.2",
+    version="1.3.3",
     description="Explanations toolbox for Tensorflow 2",
     long_description=README,
     long_description_content_type="text/markdown",
