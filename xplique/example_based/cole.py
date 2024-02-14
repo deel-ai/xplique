92056f161c21c0d706fdb0d632e0da5684264e7a
Antonin POCHE <antonin.poche@irt-systemx.fr>
2024-02-14 16:14:58 +0100

    cole: add hadamard product possibility

--- xplique/example_based/cole.py
+++ xplique/example_based/cole.py
@@ -6,13 +6,10 @@ import numpy as np
 import tensorflow as tf
 
 from ..attributions.base import BlackBoxExplainer
-from ..attributions import Saliency
 from ..types import Callable, List, Optional, Union, Type
 
 from .similar_examples import SimilarExamples
-from .projections import AttributionProjection
-from .search_methods import KNN
-from .search_methods import BaseSearchMethod
+from .projections import AttributionProjection, HadamardProjection
 
 
 class Cole(SimilarExamples):
@@ -31,19 +28,19 @@ class Cole(SimilarExamples):
     cases_dataset
         The dataset used to train the model, examples are extracted from the dataset.
         `tf.data.Dataset` are assumed to be batched as tensorflow provide no method to verify it.
-        Becareful, `tf.data.Dataset` are often reshuffled at each iteration, be sure that it is not
+        Be careful, `tf.data.Dataset` are often reshuffled at each iteration, be sure that it is not
         the case for your dataset, otherwise, examples will not make sense.
     labels_dataset
         Labels associated to the examples in the dataset. Indices should match with cases_dataset.
         `tf.data.Dataset` are assumed to be batched as tensorflow provide no method to verify it.
         Batch size and cardinality of other dataset should match `cases_dataset`.
-        Becareful, `tf.data.Dataset` are often reshuffled at each iteration, be sure that it is not
+        Be careful, `tf.data.Dataset` are often reshuffled at each iteration, be sure that it is not
         the case for your dataset, otherwise, examples will not make sense.
     targets_dataset
         Targets associated to the cases_dataset for dataset projection. See `projection` for detail.
         `tf.data.Dataset` are assumed to be batched as tensorflow provide no method to verify it.
         Batch size and cardinality of other dataset should match `cases_dataset`.
-        Becareful, `tf.data.Dataset` are often reshuffled at each iteration, be sure that it is not
+        Be careful, `tf.data.Dataset` are often reshuffled at each iteration, be sure that it is not
         the case for your dataset, otherwise, examples will not make sense.
     k
         The number of examples to retrieve. Default value is `1`.
@@ -59,6 +56,8 @@ class Cole(SimilarExamples):
     batch_size
         Number of sample treated simultaneously for projection and search.
         Ignored if `tf.data.Dataset` are provided (those are supposed to be batched).
+    device
+        Device to use for the projection, if None, use the default device.
     latent_layer
         Layer used to split the model, the first part will be used for projection and
         the second to compute the attributions. By default, the model is not split.
@@ -68,13 +67,13 @@ class Cole(SimilarExamples):
         If an `int` is provided it will be interpreted as a layer index.
         If a `string` is provided it will look for the layer name.
 
-        The method as described in the paper apply the separation on the last convolutionnal layer.
+        The method as described in the paper apply the separation on the last convolutional layer.
         To do so, the `"last_conv"` parameter will extract it.
         Otherwise, `-1` could be used for the last layer before softmax.
     attribution_method
         Class of the attribution method to use for projection.
         It should inherit from `xplique.attributions.base.BlackBoxExplainer`.
-        Ignored if a projection is given.
+        By default, it computes the gradient to make the Hadamard product in the latent space.
     attribution_kwargs
         Parameters to be passed at the construction of the `attribution_method`.
     """
@@ -89,19 +88,38 @@ class Cole(SimilarExamples):
         distance: Union[str, Callable] = "euclidean",
         case_returns: Optional[Union[List[str], str]] = "examples",
         batch_size: Optional[int] = 32,
+        device: Optional[str] = None,
         latent_layer: Optional[Union[str, int]] = None,
-        attribution_method: Type[BlackBoxExplainer] = Saliency,
+        attribution_method: Union[str, Type[BlackBoxExplainer]] = "gradient",
         **attribution_kwargs,
     ):
-        # buil attribution projection
-        projection = AttributionProjection(
-            model=model,
-            method=attribution_method,
-            latent_layer=latent_layer,
-            **attribution_kwargs,
-        )
-
         assert targets_dataset is not None
+
+        # build the corresponding projection
+        if isinstance(attribution_method, str) and attribution_method.lower() == "gradient":
+
+            operator = attribution_kwargs.get("operator", None)
+            
+            projection = HadamardProjection(
+                model=model,
+                latent_layer=latent_layer,
+                operator=operator,
+                device=device,
+            )
+        elif issubclass(attribution_method, BlackBoxExplainer):
+            # build attribution projection
+            projection = AttributionProjection(
+                model=model,
+                method=attribution_method,
+                latent_layer=latent_layer,
+                device=device,
+                **attribution_kwargs,
+            )
+        else:
+            raise ValueError(
+                f"attribution_method should be 'gradient' or a subclass of BlackBoxExplainer," +\
+                    "not {attribution_method}"
+            )
 
         super().__init__(
             cases_dataset,
