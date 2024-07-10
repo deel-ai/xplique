1e1b0c7aa00b2beab4dae9dd6d6b8897ba561a9a
Antonin POCHE <antonin.poche@irt-systemx.fr>
2024-07-10 17:08:41 +0200

    test prototypes: remove absurd tests

--- tests/example_based/test_prototypes.py
+++ tests/example_based/test_prototypes.py
@@ -6,20 +6,12 @@ import sys
 
 sys.path.append(os.getcwd())
 
-from math import prod, sqrt
-import unittest
-import time
-
-import numpy as np
 import tensorflow as tf
-
-from xplique.commons import sanitize_dataset, are_dataset_first_elems_equal
-from xplique.types import Union
 
 from xplique.example_based import Prototypes, ProtoGreedy, ProtoDash, MMDCritic
 from xplique.example_based.projections import Projection, LatentSpaceProjection
 
-from tests.utils import almost_equal, get_gaussian_data, load_data, plot, plot_local_explanation
+from tests.utils import almost_equal, get_gaussian_data, generate_model
 
 
 def test_prototypes_global_explanations_basic():
@@ -30,10 +22,9 @@ def test_prototypes_global_explanations_basic():
     k = 3
     nb_prototypes = 5
     nb_classes =  3
-
     gamma = 0.026
+
     x_train, y_train = get_gaussian_data(nb_classes=nb_classes, nb_samples_class=20)
-    x_test, y_test = get_gaussian_data(nb_classes=nb_classes, nb_samples_class=10)
 
     identity_projection = Projection(
         space_projection=lambda inputs, targets=None: inputs
@@ -90,8 +81,8 @@ def test_prototypes_local_explanations_basic():
     nb_prototypes = 5
     nb_classes =  3
     batch_size = 8
-
     gamma = 0.026
+
     x_train, y_train = get_gaussian_data(nb_classes=nb_classes, nb_samples_class=20)
     x_test, y_test = get_gaussian_data(nb_classes=nb_classes, nb_samples_class=10)
 
@@ -157,18 +148,18 @@ def test_prototypes_local_explanations_basic():
                     assert tf.reduce_all(tf.equal(labels[i, j], y_train[index]))
 
 
-def test_prototypes_global_sanity_checks_1():
+def test_prototypes_global_sanity_check():
     """
     Test prototypes global explanations sanity checks.
     
-    Check 1: For n separated gaussians, for n requested prototypes, there should be 1 prototype per gaussian.
+    Check: For n separated gaussians, for n requested prototypes, there should be 1 prototype per gaussian.
     """
 
     # Setup
     k = 3
     nb_prototypes = 3
-
     gamma = 0.026
+
     x_train, y_train = get_gaussian_data(nb_classes=nb_prototypes, nb_samples_class=20)
 
     identity_projection = Projection(
@@ -198,52 +189,6 @@ def test_prototypes_global_sanity_checks_1():
 
             # check 1
             assert len(tf.unique(prototypes_labels)[0]) == nb_prototypes
-            
-
-def test_prototypes_global_sanity_checks_2():
-    """
-    Test prototypes global explanations sanity checks.
-
-    Check 2: With local kernel_type, if there are more requested prototypes than classes, there should be at least 1 prototype per class.    
-    """
-    
-    # Setup
-    k = 3
-    nb_prototypes = 5
-    nb_classes = 3
-
-    gamma = 0.026
-    x_train, y_train = get_gaussian_data(nb_classes=nb_classes, nb_samples_class=20)
-
-    # randomize y_train
-    y_train = tf.random.shuffle(y_train)
-
-    identity_projection = Projection(
-        space_projection=lambda inputs, targets=None: inputs
-    )
-
-    for method_class in [ProtoGreedy, ProtoDash, MMDCritic]:
-        # compute general prototypes
-        method = method_class(
-            cases_dataset=x_train,
-            labels_dataset=y_train,
-            k=k,
-            projection=identity_projection,
-            batch_size=8,
-            distance="euclidean",
-            nb_prototypes=nb_prototypes,
-            kernel_type="local",
-            gamma=gamma,
-        )
-        # extract prototypes
-        prototypes_dict = method.get_global_prototypes()
-        prototypes = prototypes_dict["prototypes"]
-        prototypes_indices = prototypes_dict["prototypes_indices"]
-        prototypes_labels = prototypes_dict["prototypes_labels"]
-        prototypes_weights = prototypes_dict["prototypes_weights"]
-
-        # check 2
-        assert len(tf.unique(prototypes_labels)[0]) == nb_classes
 
 
 def test_prototypes_local_explanations_with_projection():
@@ -255,8 +200,8 @@ def test_prototypes_local_explanations_with_projection():
     nb_prototypes = 5
     nb_classes = 3
     batch_size = 8
-
     gamma = 0.026
+
     x_train, y_train = get_gaussian_data(nb_classes=nb_classes, nb_samples_class=20)
     x_train_bis, _ = get_gaussian_data(nb_classes=nb_classes, nb_samples_class=20)
     x_train = tf.concat([x_train, x_train_bis], axis=1)  # make a dataset with two dimensions
