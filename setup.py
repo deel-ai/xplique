97fd2cf79efb961e3dd7c50a0eeed23e4fa3cb9d
POCHE <antonin.poche@irt-saintexupery.com>
2024-09-25 16:05:47 +0200

    ci: restrict numpy version and specify torch test

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author="Thomas FEL",
     author_email="thomas_fel@brown.edu",
     license="MIT",
-    install_requires=['tensorflow>=2.1.0,<2.16', 'numpy', 'scikit-learn', 'scikit-image',
+    install_requires=['numpy<2.0.0', 'tensorflow>=2.1.0,<2.16.0', 'scikit-learn', 'scikit-image',
                       'matplotlib', 'scipy', 'opencv-python', 'deprecated'],
     extras_require={
         "tests": ["pytest", "pylint"],
