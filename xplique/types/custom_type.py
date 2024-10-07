46cc952b158f5c036bf7d748ff435198b40d598a
POCHE <antonin.poche@irt-saintexupery.com>
2024-10-07 17:23:52 +0200

    types: add dataset or tensor type for example based

--- xplique/types/custom_type.py
+++ xplique/types/custom_type.py
@@ -1,7 +1,13 @@
 """
 Module for custom types or signature
 """
-from typing import Callable
+from typing import Callable, TypeVar
+
+import numpy as np
 import tensorflow as tf
 
 OperatorSignature = Callable[[tf.keras.Model, tf.Tensor, tf.Tensor], float]
+
+DatasetOrTensor = TypeVar("DatasetOrTensor",
+                          tf.Tensor, np.ndarray, "torch.Tensor",
+                          tf.data.Dataset, "torch.utils.data.DataLoader")
