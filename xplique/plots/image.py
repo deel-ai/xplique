1278689a6caa0742b05498884d26065c8e1fe656
POCHE <antonin.poche@irt-saintexupery.com>
2024-10-07 17:44:01 +0200

    example based: add an image ploting function

--- xplique/plots/image.py
+++ xplique/plots/image.py
@@ -171,7 +171,7 @@ def plot_attributions(
     cols
         Number of columns.
     img_size
-        Size of each subplots (in inch), considering we keep aspect ratio
+        Size of each subplots (in inch), considering we keep aspect ratio.
     plot_kwargs
         Additional parameters passed to `plt.imshow()`.
     """
@@ -230,3 +230,102 @@ def plot_maco(image, alpha, percentile_image=1.0, percentile_alpha=80):
 
     plt.imshow(np.concatenate([image, alpha], -1))
     plt.axis('off')
+
+
+def plot_examples(
+        examples: np.ndarray,
+        distances: float = None,
+        labels: np.ndarray = None,
+        test_labels: np.ndarray = None,
+        predicted_labels: np.ndarray = None,
+        img_size: float = 2.,
+):
+    """
+    This function is for image data, it show the returns of the explain function.
+
+    Parameters
+    ---------
+    examples
+        Represente the k nearest neighbours of the input. (n, k+1, h, w, c)
+    distances
+        Distance between input data and examples.
+    labels
+        Labels of the examples.
+    labels_test
+        Corresponding to labels of the dataset test.
+    predicted_labels
+        Predicted labels of the examples.
+    img_size:
+        Size of each subplots (in inch), considering we keep aspect ratio
+    """
+    # pylint: disable=too-many-arguments
+    if distances is not None:
+        assert examples.shape[0] == distances.shape[0],\
+            "Number of samples treated should match between examples and distances."
+        assert examples.shape[1] == distances.shape[1] + 1,\
+            "Number of distances for each input must correspond to the number of examples -1."
+    if labels is not None:
+        assert examples.shape[0] == labels.shape[0],\
+            "Number of samples treated should match between examples and labels."
+        assert examples.shape[1] == labels.shape[1] + 1,\
+            "Number of labels for each input must correspond to the number of examples -1."
+
+    # number of rows depends if weights are provided
+    rows_by_input = 1
+    rows = rows_by_input * examples.shape[0]
+    cols = examples.shape[1]
+    # get width and height of our images
+    l_width, l_height = examples.shape[2:4]
+
+    # define the figure margin, width, height in inch
+    margin = 0.3
+    spacing = 0.3
+    figwidth = cols * img_size + (cols-1) * spacing + 2 * margin
+    figheight = rows * img_size * l_height/l_width + (rows-1) * spacing + 2 * margin
+
+    left = margin/figwidth
+    bottom = margin/figheight
+
+    fig = plt.figure()
+    fig.set_size_inches(figwidth, figheight)
+
+    fig.subplots_adjust(
+        left = left,
+        bottom = bottom,
+        right = 1.-left,
+        top = 1.-bottom,
+        wspace = spacing/img_size,
+        hspace= spacing/img_size * l_width/l_height
+    )
+
+    # configure the grid to show all results
+    plt.rcParams["figure.autolayout"] = True
+    plt.rcParams["figure.figsize"] = [3 * examples.shape[1], 4]
+
+    # loop to organize and show all results
+    for i in range(examples.shape[0]):
+        for k in range(examples.shape[1]):
+            plt.subplot(rows, cols, rows_by_input * i * cols + k + 1)
+
+            # set title
+            if k == 0:
+                title = "Original image"
+                title += f"\nGround Truth: {test_labels[i]}" if test_labels is not None else ""
+                title += f"\nPrediction: {predicted_labels[i, k]}"\
+                    if predicted_labels is not None else ""
+            else:
+                title = f"Example {k}"
+                title += f"\nGround Truth: {labels[i, k-1]}" if labels is not None else ""
+                title += f"\nPrediction: {predicted_labels[i, k]}"\
+                    if predicted_labels is not None else ""
+                title += f"\nDistance: {distances[i, k-1]:.4f}" if distances is not None else ""
+            plt.title(title)
+
+            # plot image
+            img = _normalize(examples[i, k])
+            if img.shape[-1] == 1:
+                plt.imshow(img[:,:,0], cmap="gray")
+            else:
+                plt.imshow(img)
+            plt.axis("off")
+    fig.tight_layout()
