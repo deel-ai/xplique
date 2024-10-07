576aab4042b24f366f802473b77b116664771f2d
POCHE <antonin.poche@irt-saintexupery.com>
2024-10-07 17:22:52 +0200

    commons: add utils for example based

--- xplique/commons/data_conversion.py
+++ xplique/commons/data_conversion.py
@@ -5,11 +5,11 @@ Numpy from/to Tensorflow manipulation
 import tensorflow as tf
 import numpy as np
 
-from ..types import Union, Optional, Tuple
+from ..types import Union, Optional, Tuple, Callable
 
 
 def tensor_sanitize(inputs: Union[tf.data.Dataset, tf.Tensor, np.ndarray],
-                    targets: Optional[Union[tf.Tensor, np.ndarray]]) -> Tuple[tf.Tensor, tf.Tensor]:
+                    targets: Union[tf.Tensor, np.ndarray]) -> Tuple[tf.Tensor, tf.Tensor]:
     """
     Ensure the output as tf.Tensor, accept various inputs format including:
     tf.Tensor, List, numpy array, tf.data.Dataset (when label = None).
@@ -66,3 +66,35 @@ def numpy_sanitize(inputs: Union[tf.data.Dataset, tf.Tensor, np.ndarray],
     """
     inputs, targets = tensor_sanitize(inputs, targets)
     return inputs.numpy(), targets.numpy()
+
+
+def sanitize_inputs_targets(explanation_method: Callable):
+    """
+    Wrap a method explanation function to ensure tf.Tensor as inputs and targets.
+    But targets may be None.
+
+    explanation_method
+        Function to wrap, should return an tf.tensor.
+    """
+    def sanitize(self,
+                 inputs: Union[tf.Tensor, np.array],
+                 targets: Optional[Union[tf.Tensor, np.array]] = None,
+                 *args,
+                 **kwargs
+                 ):
+        # pylint: disable=keyword-arg-before-vararg
+        # ensure we have tf.tensor
+        inputs = tf.cast(inputs, tf.float32)
+        if targets is not None:
+            targets = tf.cast(targets, tf.float32)
+
+        if args:
+            args = [tf.cast(arg, tf.float32) for arg in args]
+
+        if kwargs:
+            kwargs = {key: tf.cast(value, tf.float32) for key, value in kwargs.items()}
+
+        # then enter the explanation function
+        return explanation_method(self, inputs, targets, *args, **kwargs)
+
+    return sanitize
