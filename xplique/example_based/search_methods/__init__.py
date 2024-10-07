29c7fe56df3ec83ccdf79daab3bf4fe3074f4831
POCHE <antonin.poche@irt-saintexupery.com>
2024-10-07 17:30:06 +0200

    example based: introduce base search method
    Co-authored-by: Lucas Hervier <lucas.hervier@irt-saintexupery.com>

---
+++ xplique/example_based/search_methods/__init__.py
@@ -0,0 +1,12 @@
+"""
+Search methods
+"""
+
+from .base import BaseSearchMethod, ORDER
+
+# from .sklearn_knn import SklearnKNN
+from .proto_greedy_search import ProtoGreedySearch
+from .proto_dash_search import ProtoDashSearch
+from .mmd_critic_search import MMDCriticSearch
+from .knn import BaseKNN, KNN, FilterKNN
+from .kleor import KLEORSimMissSearch, KLEORGlobalSimSearch
