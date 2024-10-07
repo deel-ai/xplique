1f01962ee8fb079f5a421c8e13f7506cc3aa6d40
POCHE <antonin.poche@irt-saintexupery.com>
2024-10-07 17:44:01 +0200

    example based: add an image ploting function

--- xplique/plots/__init__.py
+++ xplique/plots/__init__.py
@@ -1,6 +1,6 @@
 """
 Utility functions to visualize explanations
 """
-from .image import plot_attributions, plot_attribution, plot_maco
+from .image import plot_attributions, plot_attribution, plot_maco, plot_examples
 from .tabular import plot_feature_impact, plot_mean_feature_impact, summary_plot_tabular
 from .timeseries import plot_timeseries_attributions
