19e434696c64a2d64dad2c8299182d1f3efa77f8
Agustin Picard <agustin.picard@gmail.com>
2025-11-07 17:30:24 +0100

    test_gsa: add first batch of tests to make sure that hsic estimators make sense

--- tests/attributions/test_hsic.py
+++ tests/attributions/test_hsic.py
@@ -1,4 +1,6 @@
+import pytest
 import numpy as np
+import tensorflow as tf
 
 from xplique.attributions import HsicAttributionMethod
 from xplique.attributions.global_sensitivity_analysis import (
@@ -63,3 +65,72 @@ def test_estimator_batch_size():
         
         stis_maps = method.explain(x, y)
         assert x.shape[:-1] == stis_maps.shape[:-1]
+
+
+# Some tests to make sure that the HSIC estimators make sense
+@pytest.mark.parametrize("Estimator", [SobolevEstimator, BinaryEstimator, RbfEstimator])
+def test_hsic_zero_when_output_constant(Estimator):
+    nb_design, H, W, C = 16, 3, 2, 1
+    masks = tf.random.uniform((nb_design, H, W, C), minval=0., maxval=1., dtype=tf.float32)
+    outputs = tf.ones((nb_design, ), dtype=tf.float32)  # constant
+    est = Estimator()
+
+    scores = est(masks, outputs, nb_design).numpy()
+    assert np.allclose(scores, 0.0, atol=1e-6)
+
+
+@pytest.mark.parametrize("Estimator", [SobolevEstimator, BinaryEstimator, RbfEstimator])
+def test_hsic_zero_when_masks_constant(Estimator):
+    nb_design, H, W, C = 12, 2, 2, 1
+    base = tf.constant(  # shape (H, W, C)
+        [
+            [[0.3], [0.7]],
+            [[0.1], [0.9]],
+        ],
+        dtype=tf.float32,
+    )
+    masks = tf.repeat(base[None, ...], repeats=nb_design, axis=0)  # (n, H, W, C)
+    outputs = tf.random.uniform((nb_design,), dtype=tf.float32)
+    est = Estimator()
+
+    scores = est(masks, outputs, nb_design).numpy()
+    assert np.allclose(scores, 0.0, atol=1e-6)
+
+
+@pytest.mark.parametrize("Estimator", [SobolevEstimator, BinaryEstimator, RbfEstimator])
+def test_hsic_invariant_to_design_permutation(Estimator):
+    nb_design, H, W, C = 24, 3, 3, 1
+    rng = tf.random.Generator.from_seed(123)
+    masks = rng.uniform((nb_design, H, W, C), dtype=tf.float32)
+    outputs = rng.uniform((nb_design,), dtype=tf.float32)
+
+    perm = tf.random.shuffle(tf.range(nb_design), seed=123)
+    masks_perm = tf.gather(masks, perm, axis=0)
+    outputs_perm = tf.gather(outputs, perm, axis=0)
+
+    est = Estimator()
+    s1 = est(masks, outputs, nb_design).numpy()
+    s2 = est(masks_perm, outputs_perm, nb_design).numpy()
+
+    assert np.allclose(s1, s2, atol=1e-6)
+
+
+@pytest.mark.parametrize("Estimator", [SobolevEstimator, BinaryEstimator, RbfEstimator])
+def test_hsic_detects_dependent_dimension(Estimator):
+    nb_design, H, W, C = 64, 2, 1, 1  # two dimensions total
+    rng = tf.random.Generator.from_seed(7)
+    dep_dim = rng.uniform((nb_design,), dtype=tf.float32)  # this drives Y
+    indep_dim = rng.uniform((nb_design,), dtype=tf.float32)
+
+    # masks[..., 0,0,0] = dep_dim, masks[..., 1,0,0] = indep_dim
+    masks = tf.stack([dep_dim, indep_dim], axis=1)  # (nb_design, 2)
+    masks = tf.reshape(masks, (nb_design, H, W, C))  # (n,2,1,1)
+
+    outputs = tf.identity(dep_dim)
+    est = Estimator()
+
+    scores = est(masks, outputs, nb_design).numpy()  # shape (W,H,C) = (1,2,1)
+    scores_hw = np.transpose(scores, (1,0,2)).reshape(H)  # back to [H] order
+    assert np.argmax(scores_hw) == 0
+    assert scores_hw[0] > scores_hw[1] + 1e-3
+
