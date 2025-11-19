42512a092988c84a0a08a5290c958d911c30812e
Frederic Boisnard <frederic.boisnard@irt-saintexupery.com>
2025-11-19 18:55:42 +0100

    viz: add plot_image_detections utility for object detection visualization
    - Add generate_heatmap() function to generate heatmaps from explanations
    - Update plot_attribution() to use generate_heatmap()
    - Add a new file dedicated to object detection plots
    
    Signed-off-by: Frederic Boisnard <frederic.boisnard@irt-saintexupery.com>

--- xplique/plots/__init__.py
+++ xplique/plots/__init__.py
@@ -2,7 +2,8 @@
 Utility functions to visualize explanations
 """
 
-from .image import plot_attribution, plot_attributions, plot_examples, plot_maco
+from .image import generate_heatmap, plot_attribution, plot_attributions, plot_examples, plot_maco
+from .object_detection import plot_image_detections, plot_images_detections
 from .tabular import plot_feature_impact, plot_mean_feature_impact, summary_plot_tabular
 from .timeseries import plot_timeseries_attributions
 
@@ -11,8 +12,11 @@ __all__ = [
     "plot_attributions",
     "plot_examples",
     "plot_maco",
+    "generate_heatmap",
     "plot_feature_impact",
     "plot_mean_feature_impact",
     "summary_plot_tabular",
     "plot_timeseries_attributions",
+    "plot_image_detections",
+    "plot_images_detections",
 ]
