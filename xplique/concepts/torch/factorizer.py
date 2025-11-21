2197c344effb27602324d33c9393693bec9d1bee
Frederic Boisnard <frederic.boisnard@irt-saintexupery.com>
2025-11-21 19:43:13 +0100

    feat: add support for Overcomplete factorizer
    Signed-off-by: Frederic Boisnard <frederic.boisnard@irt-saintexupery.com>

--- xplique/concepts/torch/factorizer.py
+++ xplique/concepts/torch/factorizer.py
@@ -71,3 +71,53 @@ class TorchSklearnNMFFactorizer(SklearnNMFFactorizer):
         )
         
         return coefficients @ concept_bank_tensor
+
+
+class OvercompleteFactorizer:
+
+    def __init__(self, optimizer_class, nb_concepts, device='cuda', **kwargs):
+        self._nmf = optimizer_class(nb_concepts=nb_concepts, device=device, **kwargs)
+        self.device = device
+
+    def fit(self, activations: np.ndarray):
+        activations_torch = torch.tensor(activations, device=self.device)
+        result = self._nmf.fit(activations_torch)
+        # Handle both tuple return (Z, D) and single tensor return
+        if isinstance(result, tuple):
+            Z_torch, D_torch = result[0], result[1]
+        else:
+            Z_torch = result
+            D_torch = self._nmf.get_dictionary()
+
+        concept_bank_w = D_torch.detach().cpu().numpy()
+        coeffs_u = Z_torch.detach().cpu().numpy()
+        return concept_bank_w, coeffs_u
+
+    def encode(self, activations: np.ndarray) -> np.ndarray:
+        activations_torch = torch.tensor(activations, device=self.device)
+        result = self._nmf.encode(activations_torch)
+        return result.detach().cpu().numpy()
+
+    def encode_differentiable(self, activations: torch.Tensor) -> torch.Tensor:
+        return self._nmf.encode(activations)
+
+    def decode(self, coefficients):
+        if isinstance(coefficients, np.ndarray):
+            coefficients_torch = torch.tensor(coefficients, device=self.device)
+            result = self._nmf.decode(coefficients_torch)
+            return result.detach().cpu().numpy()
+        else:
+            return self._nmf.decode(coefficients)
+
+    def get_concept_bank(self) -> np.ndarray:
+        dictionary = self._nmf.get_dictionary()
+        return dictionary.detach().cpu().numpy()
+
+    @property
+    def is_fitted(self) -> bool:
+        return self._nmf.fitted
+
+    @property
+    def requires_positive_activations(self) -> bool:
+        from overcomplete.optimization import SemiNMF
+        return not isinstance(self._nmf, SemiNMF)
