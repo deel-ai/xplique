9a610c749c718d1019767889ce75c93595f2bcfe
Agustin Picard <agustin.picard@gmail.com>
2025-12-03 10:20:31 +0100

    Bump version: 1.4.0 → 1.4.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as fh:
 
 setup(
     name="Xplique",
-    version="1.4.0",
+    version="1.4.1",
     description="Explanations toolbox for Tensorflow 2",
     long_description=README,
     long_description_content_type="text/markdown",
