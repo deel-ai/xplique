fidelity: actually fix average before abs

xplique/metrics/fidelity.py
@@ -586,9 +586,9 @@ class BaseAverageXMetric(ExplanationMetric, ABC):
         exp = tf.convert_to_tensor(explanations, dtype=tf.float32)
 
         # Absolute value and channel averaging
-        exp = tf.math.abs(exp)
-        if exp.shape.rank == 4:
-            mask = tf.reduce_mean(exp, axis=-1)  # (B,H,W)
+        mask = tf.math.abs(exp)
+        if mask.shape.rank == 4:
+            mask = tf.reduce_mean(mask, axis=-1)  # (B,H,W)
 
         # Apply min-max normalization per sample
         axes = tf.range(1, tf.rank(mask))